e4e25d9cc7fd5aab1fc74b7448a742391f065506
david-castaneda <dcastane@cs.fiu.edu>
2017-11-02 21:17:28 -0400

    what is section mobile compatible

--- src/components/WhatLike.jsx
+++ src/components/WhatLike.jsx
@@ -12,16 +12,16 @@ class WhatLike extends React.Component{
             <div className="whatlike">
                 <div className="content color-change">
                     <header> What is it like?</header>
-                        <iframe width="720" height="510" src="https://www.youtube.com/embed/iuPGCi9VEco" frameborder="0" gesture="media" allowfullscreen></iframe> 
+                        <iframe id="mango-video" width="720" height="510" src="https://www.youtube.com/embed/iuPGCi9VEco" frameborder="0" gesture="media" allowfullscreen></iframe> 
                         <div className="video-wrapper">
                             <div className="row">
                                 <div className="col">
-                                    <iframe width="420" height="250" src="https://www.youtube.com/embed/qj4DQfYx2Hg" frameborder="0" gesture="media" allowfullscreen></iframe>  
+                                    <iframe id="hackathon-video" width="420" height="250" src="https://www.youtube.com/embed/qj4DQfYx2Hg" frameborder="0" gesture="media" allowfullscreen></iframe>  
                                 </div>
                             </div>           
                             <div className="row">
                                 <div className="col">
-                                    <iframe width="420" height="250" src="https://www.youtube.com/embed/fcsugf8I0ms" frameborder="0" gesture="media" allowfullscreen></iframe>  
+                                    <iframe id="mlh-video" width="420" height="250" src="https://www.youtube.com/embed/fcsugf8I0ms" frameborder="0" gesture="media" allowfullscreen></iframe>  
                             </div>           
                         </div>
                     </div> 
