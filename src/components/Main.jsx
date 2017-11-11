eb70c13f6d34a837bc74d746a0c598bdfc505b9f
david-castaneda <dcastane@cs.fiu.edu>
2017-11-10 19:10:41 -0500

    style change initiated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 
 import $ from'jquery';
 
-import logo from "../public/img/mangos/green-mango.svg";
+import imaginemango from "../public/img/mangos/imagine-mango.svg";
+import innovatemango from "../public/img/mangos/inspire-mango.svg";
+import inspiremango from "../public/img/mangos/innovate-mango.svg";
 
 // CSS
 import pill from '../public/css/pill.css';
@@ -36,9 +38,58 @@ class Main extends React.Component {
   render() {
     // Jquery
     $(document).ready(function() {
-      var colors = ["linear-gradient(233deg, #ffc400, #c1f523)","linear-gradient(233deg, #6800ff, #f52376)", "linear-gradient(233deg, #efff92, #d2fcf9)"];
-      var rand = Math.floor(Math.random() * colors.length);
-      $('.change').css("background", colors[rand]);
+      var liststyle = [ 'imagine','innovate','inspire'];
+      var style = liststyle[Math.floor(Math.random() * liststyle.length)];
+
+      if(style == 'imagine'){
+        // BG Styles
+         $('.style').css('background','linear-gradient(233deg, #ffc400, #c1f523');
+         $('.style').css('color','#000');
+         $('.modal-btn').css('color','#000');
+         $('.modal-btn').css('border','1px solid #000');
+         $('.modal-btn').hover(function(e){
+           $(this).css('background', e.type === 'mouseenter' ? "#000":'transparent');
+           $(this).css('color',e.type === 'mouseenter' ? '#fff':'#000');
+         });
+
+        // Text Style
+        $('.title').text('Imagine.');
+        $('.title').css('color','#C8F020')
+
+        // Logo Style
+      }
+
+      if(style == 'inspire'){
+        $('.style').css('background','linear-gradient(233deg, #efff92, #d2fcf9');
+        $('.style').css('color','#000');
+        $('.modal-btn').css('color','#000');
+        $('.modal-btn').css('border','1px solid #000');
+        $('.modal-btn').hover(function(e){
+          $(this).css('background', e.type === 'mouseenter' ? "#000":'transparent');
+          $(this).css('color',e.type === 'mouseenter' ? '#fff':'#000');
+        });
+
+
+        // Text Style
+        $(".title").text('Inspire.');
+        $('.title').css('color','#23F5E9');
+      }
+
+      if(style == 'innovate'){
+        $('.style').css('background','linear-gradient(233deg, #6800ff, #f52376');
+        $('.style').css('color','#fff');
+        $('.modal-btn').css('color','#fff');
+        $('.modal-btn').css('border','1px solid #fff');
+        $('.modal-btn').hover(function(e){
+          $(this).css('background', e.type === 'mouseenter' ? "#fff":'transparent');
+          $(this).css('color',e.type === 'mouseenter' ? '#000':'white');
+        });
+
+
+        // Text Style
+        $('.title').text('Innovate.');
+        $('.title').css('color','#4652F7')
+      }
   });
 
   $(document).ready(function(){$("div").hide().fadeIn(1000);});
@@ -46,23 +97,23 @@ class Main extends React.Component {
     return (
       <div class="">
         <div className="pill-move">
-           <div className="pill" id="pill-1"></div>
+           {/* <div className="pill" id="pill-1"></div>
            <div className="pill" id="pill-2"></div>
            <div className="pill" id="pill-3"></div>
-           <div className="pill" id="pill-4"></div>           
+           <div className="pill" id="pill-4"></div>            */}
         </div>
 
         <div className="pill-move top-pills">
-           <div className="pill" id="pill-5"></div>
+           {/* <div className="pill" id="pill-5"></div>
            <div className="pill" id="pill-6"></div>
            <div className="pill" id="pill-7"></div>
-           <div className="pill" id="pill-8"></div>           
+           <div className="pill" id="pill-8"></div>            */}
         </div>
 
         <div className="macbook">
           <div className="hero">
-            <img className="mango" src={logo} alt="mangologo"/>
-              <h1 className="innovate">Imagine.</h1>
+            <img className="mango" src={imaginemango} alt="mangologo"/>
+              <h1 className="title"></h1>
               </div>
           </div>
           <div className="hero">
@@ -82,7 +133,7 @@ class Main extends React.Component {
           </div>
   
           <div className="whatis">
-              <div className="content change">
+              <div className="content style">
                       <header> What is <br></br>a Hackathon?</header>
                       <p>MangoHacks is a chance to meet new people, learn something, make something, dream along, and have fun.</p>
                       <p>Everyone is welcomed - from the most experienced hackers, designers, and builders to the thinkers and the curious who have never heard of a hackathon. Regardless of your experience, there is something for you at MangoHacks.</p>
