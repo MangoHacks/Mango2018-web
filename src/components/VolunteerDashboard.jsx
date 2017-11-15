b5d90c0445565e04add6c8f6b19209b65aada32f
Soya Diaoune <soya@notebear.com>
2017-11-15 15:11:49 -0500

    added modal for adding user (volunteer)

--- src/components/VolunteerDashboard.jsx
+++ src/components/VolunteerDashboard.jsx
@@ -1,5 +1,34 @@
 
 import React, { Component } from 'react';
+import Modal from 'react-modal';
+
+let DOMPurify = require('../dist/purify.js');
+
+const modalStyle = {
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(255, 255, 255, 0.75)'
+  },
+  content : {
+    position                   : 'absolute',
+    top                        : '40px',
+    left                       : '40px',
+    right                      : '40px',
+    bottom                     : '40px',
+    border                     : '1px solid #ccc',
+    background                 : '#fff',
+    overflow                   : 'auto',
+    WebkitOverflowScrolling    : 'touch',
+    borderRadius               : '4px',
+    outline                    : 'none',
+    padding                    : '20px'
+
+  }
+}
 
 class VolunteerDashboard extends Component {
 
@@ -7,12 +36,67 @@ class VolunteerDashboard extends Component {
     super(props);
     this.state = {
       list: [],
+      name: "",
+      email: '',
+      school: '',
+      major: '',
+      year: 'Freshman',
+      firsttime: '',
+      gender: 'Male',
+      size: '',
+      github: '',
+      resume: [],
+      diet: '',
+      mlh: ''
     };
+    this.signup = this.signup.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.editUser = this.editUser.bind(this);
     this.checkIn= this.checkIn.bind(this);
 
     this.renderVolunteerDashboard = this.renderVolunteerDashboard.bind(this);
+  }
+
+  signup(event) {
+    event.preventDefault();
+
+    let cleanName = DOMPurify.sanitize(this.state.name);
+    let cleanEmail = DOMPurify.sanitize(this.state.email);
+    let cleanSchool = DOMPurify.sanitize(this.state.school);
+    let cleanMajor = DOMPurify.sanitize(this.state.major);
+    let cleanYear = DOMPurify.sanitize(this.state.year);
+    let cleanFirstTime = DOMPurify.sanitize(this.state.firsttime);
+    let cleanGender = DOMPurify.sanitize(this.state.gender);
+    let cleanSize = DOMPurify.sanitize(this.state.size);
+    let cleanGithub = DOMPurify.sanitize(this.state.github);
+    // let cleanResume = DOMPurify.sanitize(this.state.resume);
+    let resume = (this.state.resume);
+
+    let cleanDiet = DOMPurify.sanitize(this.state.diet);
+    let cleanMLH = DOMPurify.sanitize(this.state.mlh);
+
+    fetch('http://localhost:8050/form/', {
+      method: 'POST',
+      "headers": {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name: cleanName,
+        email: cleanEmail,
+        school: cleanSchool,
+        major: cleanMajor,
+        year: cleanYear,
+        firsttime: cleanFirstTime,
+        gender: cleanGender,
+        size: cleanSize,
+        github: cleanGithub,
+        resume: resume,
+        diet: cleanDiet,
+        mlh: cleanMLH
+      })
+    });
+    alert('Thanks for registering to MangoHacks')
+    window.location.href = "http://localhost:3000/signup";
   }
   componentDidMount() {
     this.renderVolunteerDashboard();
@@ -93,6 +177,139 @@ console.log('yay' + id)
     return (
 
       <div className="dashboard-body" align="center">
+
+        <Modal
+          isOpen={true}
+          aria={{
+            labelledby: "heading",
+            describedby: "full_description"
+          }}
+          style = {modalStyle}
+          className="dashboard-modal">
+          <div align="center">
+          <h1 id="heading">Add Hacker</h1>
+          <div id="full_description" align="center">
+            <p>Last minute sign up</p>
+          </div>
+
+          <form action="/form" className="" onSubmit={this.signup} encType="multipart/form-data">
+            <div className="form-group">
+              <input type="name" name="name" className="name" placeholder="Name" required value={this.state.name} onChange={this.handleInputChange} />
+            </div>
+            <div className="form-group">
+              <input type="email" name="email" className="email" required value={this.state.email} onChange={this.handleInputChange} id="email" placeholder="Email" />
+            </div>
+            <div className="form-group">
+              <input type="text" list="schools" name="school" className="school" required value={this.state.school} onChange={this.handleInputChange} id="school" placeholder="School" />
+            </div>
+            <datalist id="schools">
+              <option value="Florida International University" />
+            </datalist>
+
+            <div className="form-group">
+              <input type="text" list="majors" name="major" className="major" required value={this.state.major} onChange={this.handleInputChange} id="major" placeholder="Major" />
+            </div>
+            <datalist id="majors">
+              <option value="Computer Science" />
+              <option value="Computer Engineering" />
+              <option value="Information Technology" />
+              <option value="Business" />
+              <option value="Electrical Engineering" />
+            </datalist>
+            {/*
+                  <div className="form-group">
+                    <label htmlFor="">Freshman</label>
+                    <input type="checkbox" value="Freshman" checked={this.state.year === 'Freshman'} onChange={this.handleYearChange} />
+                    <label htmlFor="">Sophmore</label>
+                    <input type="checkbox" value="Sophmore" checked={this.state.year === 'Sophmore'} onChange={this.handleYearChange} />
+                    <label htmlFor="">Junior</label>
+                    <input type="checkbox" value="Junior" checked={this.state.year === 'Junior'} onChange={this.handleYearChange} />
+                    <label htmlFor="">Senior</label>
+                    <input type="checkbox" value="Senior" checked={this.state.year === 'Senior'} onChange={this.handleYearChange} />
+                  </div> */}
+
+            <label htmlFor="">Grade Level</label>
+            <div class="form-group">
+              <select name="year" onChange={this.handleYearChange}>
+                <option value="Freshman">Freshman</option>
+                <option value="Sophmore">Sophmore</option>
+                <option value="Junior">Junior</option>
+                <option value="Senior">Senior</option>
+                <option value="">Super Senior</option>
+                <option value="">DAMN</option>
+              </select>
+            </div>
+
+            <label htmlFor="">Dietary Restrictions</label>
+            <div className="form-group">
+              <input type="text" name="diet" className="diet" required placeholder="none" value={this.state.diet} onChange={this.handleInputChange} />
+            </div>
+
+            <label htmlFor="">First Time?</label>
+            <div className="form-group">
+              <label htmlFor="">Yes</label>
+              <input type="checkbox" value="Yes" checked={this.state.firsttime === 'Yes'} onChange={this.handleFirstTimeChange} />
+              <label htmlFor="">No</label>
+              <input type="checkbox" value="No" checked={this.state.firsttime === 'No'} onChange={this.handleFirstTimeChange} />
+            </div>
+
+
+
+            <label htmlFor="">Gender</label>
+            <div className="form-group">
+              <select value="gender" onChange={this.handleGenderChange}>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Other">Other</option>
+              </select>
+            </div>
+
+            <label htmlFor="">Shirt Size</label>
+            <div class="form-group">
+              <select name="size" onChange={this.handleSizeChange}>
+                <option value="Small">Small</option>
+                <option value="Medium">Medium</option>
+                <option value="Large">Large</option>
+                <option value="X-Large">X-Large</option>
+              </select>
+            </div>
+
+            <div className="github">
+              <span>http://</span>
+              <input type="url" name="github" required value={this.state.github} onChange={this.handleInputChange} id="github" placeholder="github.com/octocat" />
+            </div>
+            <br />
+            <div className="resume">
+              <input type="file" name="resume" value={this.state.resume} onChange={this.handleFileChange} />
+            </div>
+            {/* <section>
+        <div className="dropzone">
+          <Dropzone onDrop={this.onDrop.bind(this)}>
+            <p>Try dropping some files here, or click to select files to upload.</p>
+          </Dropzone>
+        </div>
+        <aside>
+          <h2>Dropped files</h2>
+          <ul>
+            {
+              this.state.resume.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+            }
+          </ul>
+        </aside>
+      </section> */}
+
+            <div className="form-group">
+              <input type="checkbox" value="Agreed" required checked={this.state.mlh === 'Agreed'} onChange={this.handleMLHChange} />I agree to <a href=""> MLH Code of Conduct </a>
+            </div>
+
+            <button className="btn btn-default" type="submit">✓ Register</button>
+          </form>
+
+
+          </div>
+        </Modal>
+
+
         <div className="dashboard-menu" align="left">
           <p>MangoHacks</p>
         </div>
