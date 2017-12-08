356cd441f76751ed21cbad77529cf6f576754e4c
sdiaoune <sdiao002@fiu.edu>
2017-12-07 20:07:28 -0500

    button to signup

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -263,7 +263,7 @@ class Main extends React.Component {
             <img className="mango" src={imaginemango} alt="mangologo" />
             <h1 className="title"></h1>
             <div align="center">
-              <button className="signupbutton">Sign Up!</button>
+              <a href="/signup"><button className="signupbutton">Sign Up!</button></a>
             </div>
             <img className="bg-hero" src={motherboard} alt="bg-picture" />
 
